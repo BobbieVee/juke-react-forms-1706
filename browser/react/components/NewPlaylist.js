e3535afd0735aa64a7258fb25ebcd547cb0c71e6
Robert van der Mark <robert.vandermark@gmail.com>
2017-09-15 11:47:55 -0400

    files from different version moved to Bak folder

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class NewPlaylist extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      input: ''
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput(e){
+    const input = e.target.value;
+    this.setState({input});
+    event.preventDefault();
+  }
+
+  handleSubmit(){
+    const input = this.state.input;
+    axios.post('/api/playlists', { name: input })
+    .then( () => {
+      this.setState({input: ''})
+    })
   }
 
   render() {
+    const input = this.state.input;
     return (
       <div className="well">
-        <form className="form-horizontal">
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
           <fieldset>
             <legend>New Playlist</legend>
             <div className="form-group">
               <label className="col-xs-2 control-label">Name</label>
               <div className="col-xs-10">
-                <input className="form-control" type="text"/>
+                <input className="form-control" type="text" value={input} onChange={this.handleInput}/>
               </div>
             </div>
             <div className="form-group">
